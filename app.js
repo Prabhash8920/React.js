eaffbd26827364f9e3310eaded1cdf24f95952f7
Prabhash kumar <prabhashkumar7835@gmail.com>
2025-12-14 10:14:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//export var a =20
+
+// export-2
+// named
+// deafult
+
+// export const arr = [10,20,30,40]
+
+// const user = "Sarthak"
+
+// export default  user
+
+const user = 'Sarthak'
+const age = 89
+const skills =['frontend' , 'Backend', 'UI/UX']
+export default user
---
+++ script.js
@@ -0,0 +1,21 @@
+// Library 
+// -GSAP // aNIMATIONS LIBRARY FOR moving animatiions
+// -Lenis // for smooth scrolling
+// -ReactJS // for website UI
+
+
+// Framework
+// -NextJS
+// -Angular
+// -Remix
+
+// import username from './app.js'
+// import{arr} from './app.js'
+
+// console.log(username)
+
+import naam from './app.js'
+import user2 from './test.js'
+
+
+console.log(naam,user2);
